352dd84350dd42fe1ce0b7de287e3ad007e9dfc9
Christopher Ford <chris.ford@oneserve.co.uk>
2016-04-28 13:39:27 +0100

    Added remainder of the creature images, renamed messages.js to weatherInfo.js

--- js/weatherInfo.js
+++ js/weatherInfo.js
@@ -7,7 +7,7 @@ var weatherFeedback = [
         "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "abs-zero.jpg",
         "warning": "Beware Ice Elementals traveller, for only they can survive in conditions such as these.",
-        "creature": null,
+        "creature": "ice-elemental.png",
         "comment": "Unless you are protected by great magics, you will be dead the moment you step outside."
     },
 
@@ -57,7 +57,7 @@ var weatherFeedback = [
         "filter": "linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1))",
         "background": "titan.jpg",
         "warning": "The spirits of the nether abound on days such as these. Be wary, for whilst you pass through them, they can surely do damage to you.",
-        "creature": null,
+        "creature": "spirit.png",
         "comment": "These are almost perfect adventuring conditions. Your armour will keep you warm, just be sure that your sword arm doesn't get chilled."
     },
 
@@ -107,7 +107,7 @@ var weatherFeedback = [
         "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "temperate.jpg",
         "warning": "In these climes, the Bogleech amass in numbers. Be wary.",
-        "creature": null,
+        "creature": "bogleech.png",
         "comment": "On a day such as this, a canny adventurer would actually seek out the dungeons rather than avoid them."
     },
 
@@ -127,7 +127,7 @@ var weatherFeedback = [
         "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "jungle.jpg",
         "warning": "When the air outside begins to match that of the upper levels of the nine hells, minor devils will begin to venture out into the world.",
-        "creature": "bonedevil.png",
+        "creature": "devil.png",
         "comment": "Be sure to bring plenty of water skins on your travels, adventurer. On a day such as this, you will need them."
     },
 
@@ -137,7 +137,7 @@ var weatherFeedback = [
         "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "dragon-hot.jpg",
         "warning": "This is a perfect day for meeting Fire Giants, traveller. Be mindful of the earth-shaking that signals their approach.",
-        "creature": null,
+        "creature": "fire-giant.png",
         "comment": "If travelling on this day adventurer, be sure to have a frost mage in your party."
     },
 
@@ -147,7 +147,7 @@ var weatherFeedback = [
         "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "desert-golem.jpg",
         "warning": "Surely this day will bring you desert travel, in climes such as these. Be mindful of Sand Golems.",
-        "creature": null,
+        "creature": "sand-golem.png",
         "comment": "When the temperatures climb this high adventurer, might I suggest some magical armour as an alternative to leather and steel?"
     },
 
