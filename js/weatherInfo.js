438cc95efacb2795c3186302a65815afde40487e
Christopher Ford <c.d.j.ford@gmail.com>
2016-05-01 12:38:26 +0100

    Removed the filter from weather objects in favour of applying them directly to the images

--- js/weatherInfo.js
+++ js/weatherInfo.js
@@ -4,7 +4,6 @@ var weatherFeedback = [
     {
         "min-temp": -273.1,
         "max-temp": -50.0,
-        "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "abs-zero.jpg",
         "warning": "Beware Ice Elementals traveller, for only they can survive in conditions such as these.",
         "creature": "ice-elemental.png",
@@ -14,7 +13,6 @@ var weatherFeedback = [
     {
         "min-temp": -50.0,
         "max-temp": -30.0,
-        "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "ice-dragon.jpg",
         "warning": "Be wary traveller, for this is weather perfect for Frost Giants.",
         "creature": "frost-giant.png",
@@ -24,7 +22,6 @@ var weatherFeedback = [
     {
         "min-temp": -30.0,
         "max-temp": -15.0,
-        "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "snow-battle.jpg",
         "warning": "You would do well to watch your back this day adventurer, for surely these conditions are exactly those preferred by the Yeti as he stalks his prey.",
         "creature": "yeti.png",
@@ -34,7 +31,6 @@ var weatherFeedback = [
     {
         "min-temp": -15.0,
         "max-temp": -5.0,
-        "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "graveyard.jpg",
         "warning": "Traveller, be mindful of your surroundings. On days such as these, Ice Mephits lie in wait of unwary adventurers.",
         "creature": "ice-mephit.png",
@@ -44,7 +40,6 @@ var weatherFeedback = [
     {
         "min-temp": -5.0,
         "max-temp": 0.0,
-        "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "cold.jpg",
         "warning": "I should advise against forest travel in such conditions, lest you want to find yourself tracked by Frost Wolves.",
         "creature": "frost-wolf.png",
@@ -54,7 +49,6 @@ var weatherFeedback = [
     {
         "min-temp": 0.0,
         "max-temp": 5.0,
-        "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "titan.jpg",
         "warning": "The spirits of the nether abound on days such as these. Be wary, for whilst you pass through them, they can surely do damage to you.",
         "creature": "spirit.png",
@@ -64,7 +58,6 @@ var weatherFeedback = [
     {
         "min-temp": 5.0,
         "max-temp": 10.0,
-        "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "stormy-skies.jpg",
         "warning": "Although some think it too cold, I have seen Chimeras on days such as this with mine own eyes. Be watchful of the skies.",
         "creature": "chimera.png",
@@ -74,7 +67,6 @@ var weatherFeedback = [
     {
         "min-temp": 10.0,
         "max-temp": 15.0,
-        "filter": "linear-gradient(rgba(0, 0, 255, 0.1), rgba(0, 0, 255, 0.1))",
         "background": "floating-rocks.jpg",
         "warning": "Traveller, be wary of Owlbears in the woods. They oft hunt on days such as these.",
         "creature": "owl-bear.png",
@@ -84,7 +76,6 @@ var weatherFeedback = [
     {
         "min-temp": 15.0,
         "max-temp": 20.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "mild.jpg",
         "warning": "Beware, for the fearsome Beholder abound in conditions such as these.",
         "creature": "beholder.png",
@@ -94,7 +85,6 @@ var weatherFeedback = [
     {
         "min-temp": 20.0,
         "max-temp": 25.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "forest.jpg",
         "warning": "Be mindful of the treants this day adventurer, for they are not as friendly as they would have you believe.",
         "creature": "treant.png",
@@ -104,7 +94,6 @@ var weatherFeedback = [
     {
         "min-temp": 25.0,
         "max-temp": 30.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "temperate.jpg",
         "warning": "In these climes, the Bogleech amass in numbers. Be wary.",
         "creature": "bogleech.png",
@@ -114,7 +103,6 @@ var weatherFeedback = [
     {
         "min-temp": 30.0,
         "max-temp": 35.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "bog.jpg",
         "warning": "On these warm days, the slimes will come up from the bogs. Beware their suffocating grasp.",
         "creature": "slime.png",
@@ -124,7 +112,6 @@ var weatherFeedback = [
     {
         "min-temp": 35.0,
         "max-temp": 40.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "jungle.jpg",
         "warning": "When the air outside begins to match that of the upper levels of the nine hells, minor devils will begin to venture out into the world.",
         "creature": "devil.png",
@@ -134,7 +121,6 @@ var weatherFeedback = [
     {
         "min-temp": 40.0,
         "max-temp": 50.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "dragon-hot.jpg",
         "warning": "This is a perfect day for meeting Fire Giants, traveller. Be mindful of the earth-shaking that signals their approach.",
         "creature": "fire-giant.png",
@@ -144,7 +130,6 @@ var weatherFeedback = [
     {
         "min-temp": 50.0,
         "max-temp": 70.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "desert-golem.jpg",
         "warning": "Surely this day will bring you desert travel, in climes such as these. Be mindful of Sand Golems.",
         "creature": "sand-golem.png",
@@ -154,7 +139,6 @@ var weatherFeedback = [
     {
         "min-temp": 70.0,
         "max-temp": 100.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "superhot.jpg",
         "warning": "In temperatures such as these, the Fire Succubi thrive. Listen not to their song, lest you want to meet a truly firey demise.",
         "creature": "succubus.png",
@@ -164,7 +148,6 @@ var weatherFeedback = [
     {
         "min-temp": 100.0,
         "max-temp": 1000000.0,
-        "filter": "linear-gradient(rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.1))",
         "background": "plain-of-fire.jpg",
         "warning": "In a world such as this, beware the fury of the Balrog.",
         "creature": "balrog.png",
